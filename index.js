0c060f7faef6525c6792b502da06d13d3b1a40dc
Anshuman Verma <anshu.av97@gmail.com>
2018-12-24 02:01:54 +0530

    Open links in browser, fix args as I gotta sleep

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const alfy = require('alfy');
 (async () => {
 
 	const data = await alfy.fetch(`https://api.github.com/users/${alfy.input}/events?access_token=${process.env.access_token}`)
-	const items =	data.map(event => ({
+	const items =	data.map(event => {
+		const arg;
+		if(event.payload.action == 'created') {
+			arg = event.payload.comment.html_url
+		}	else {
+			arg = event.payload.actor.url
+		}
+		return {
 			title: `${event.type} - ${event.repo.name}`,
 			subtitle: `${event.payload.action} - ${event.created_at}`,
 			arg: event.actor.url
-		}));
+		}});
 
 		// console.log(process.env.access_token)
 		// console.log(alfy.input, items, data)
