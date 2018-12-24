121149cded927f0cdcd57649b01b1ed57137967d
Anshuman Verma <anshu.av97@gmail.com>
2018-12-24 12:24:43 +0530

    AHandle more events

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const alfy = require('alfy');
 determineArg = (event) => {
 	if (event.payload.action === 'created' || event.type === 'CommitCommentEvent') {
 		return event.payload.comment.html_url
-	}	else if (['opened', 'closed'].includes(event.payload.action)) {
-		return (event.payload.pull_request || event.payload.issue)['html_url']
-	} else if (event.type === 'WatchEvent') {
-		return event.repo.url
+	}	else if (['opened', 'closed', 'published'].includes(event.payload.action)) {
+		return (event.payload.pull_request || event.payload.issue || event.payload.release)['html_url']
+	} else if (['WatchEvent', 'DeleteEvent', 'CreateEvent', 'PublicEvent'].includes(event.type)) {
+		return `https://github.com/${event.repo.url.split('/').slice(4,6).join('/')}`
 	} else if (event.type === 'ForkEvent') {
 		return event.payload.forkee.html_url
 	} else if (event.type === 'PushEvent') {
