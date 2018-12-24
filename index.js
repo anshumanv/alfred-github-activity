3a47f9d4d3b288caf558ed243f7fc21250013869
Anshuman Verma <anshu.av97@gmail.com>
2018-12-24 11:43:11 +0530

    RRefactor into a separate function to determine arg

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const alfy = require('alfy');
 
+determineArg = (event) => {
+	if(event.payload.action === 'created') {
+		return event.payload.comment.html_url
+	}	else if(['opened', 'closed'].includes(event.payload.action)) {
+		return (event.payload.pull_request || event.payload.issue)['html_url']
+	} else {
+		return event.actor.url
+	}
+}
+
 (async () => {
 
 	const data = await alfy.fetch(`https://api.github.com/users/${alfy.input}/events?access_token=${process.env.access_token}`)
 	const items =	data.map(event => {
-		const arg;
-		if(event.payload.action == 'created') {
-			arg = event.payload.comment.html_url
-		}	else {
-			arg = event.payload.actor.url
-		}
+		// Determine the URL to open
+
+		const arg = determineArg(event)
+
 		return {
 			title: `${event.type} - ${event.repo.name}`,
 			subtitle: `${event.payload.action} - ${event.created_at}`,
-			arg: event.actor.url
+			arg
 		}});
 
 		// console.log(process.env.access_token)
-		// console.log(alfy.input, items, data)
+		// console.log(alfy.input, items)
 		alfy.output(items)
 	})();
