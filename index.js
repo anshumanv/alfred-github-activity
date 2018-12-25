86afa72eacbb1e942f4e9eb5c67f89d9ebafdbb1
Anshuman Verma <anshu.av97@gmail.com>
2018-12-25 11:47:01 +0530

    Heavy refactor

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
 const alfy = require('alfy');
 
+// Determine the URL to open on selection of an output item.
 determineArg = (event) => {
+	const { payload: { action, comment, issue, pull_request, forkee, release }, type, repo, head, actor } = event;
 	if (event.payload.action === 'created' || event.type === 'CommitCommentEvent') {
-		return event.payload.comment.html_url
-	}	else if (['opened', 'closed', 'published'].includes(event.payload.action)) {
-		return (event.payload.pull_request || event.payload.issue || event.payload.release)['html_url']
-	} else if (['WatchEvent', 'DeleteEvent', 'CreateEvent', 'PublicEvent'].includes(event.type)) {
-		return `https://github.com/${event.repo.name}`
-	} else if (event.type === 'ForkEvent') {
-		return event.payload.forkee.html_url
-	} else if (event.type === 'PushEvent') {
-		return `https://github.com/${event.repo.name}/commits/${event.payload.head}`
+		return comment.html_url
+	}	else if (['opened', 'closed', 'published'].includes(action)) {
+		return (pull_request || issue || release)['html_url']
+	} else if (['WatchEvent', 'DeleteEvent', 'CreateEvent', 'PublicEvent'].includes(type)) {
+		return `https://github.com/${repo.name}`
+	} else if (type === 'ForkEvent') {
+		return forkee.html_url
+	} else if (type === 'PushEvent') {
+		return `https://github.com/${repo.name}/commits/${head}`
 	} else {
 		// Temporary
-		return event.actor.url
+		return actor.url
 	}
 }
 
+
+// Determine the action of the event
 determineAction = event => {
-	const { payload } = event
-	if (event.type === 'PushEvent') {
-		return payload.ref
-	} else if (event.type === 'ForkEvent') {
+	const { payload: {ref, ref_type, action}, type } = event
+	if (type === 'PushEvent') {
+		return ref
+	} else if (type === 'ForkEvent') {
 		return 'Forked at'
-	} else if (['CreateEvent', 'DeleteEvent'].includes(event.type)) {
-		return `${payload.ref_type} - ${payload.ref}`
-	} else return payload.action || ''
+	} else if (['CreateEvent', 'DeleteEvent'].includes(type)) {
+		return `${ref_type} - ${ref}`
+	} else return action || ''
 }
 
+// Script to run on changing input
 (async () => {
 
 	const data = await alfy.fetch(`https://api.github.com/users/${alfy.input}/events?access_token=${process.env.access_token}`)
 	const items =	data.map(event => {
 		// Determine the URL to open
-
 		const arg = determineArg(event)
+
+		// Determine initial of subtitle
 		const preSub = determineAction(event)
+
+		// New date object for the action time
 		const actionTimestamp = new Date(event.created_at)
 
+		// Output items
 		return {
 			title: `${event.type} - ${event.repo.name}`,
 			subtitle: `${preSub} - ${actionTimestamp}`,
 			arg,
-			quicklookUrl: arg,
+			quicklookUrl: arg,	// This is saikou
 		}});
 
-		// console.log(process.env.access_token)
-		// console.log(alfy.input, items)
+		// Throw the final list of items
 		alfy.output(items)
-	})();
+})();
