d1e1437df847566dc4aa1e143ec18169c0de1880
Anshuman Verma <anshu.av97@gmail.com>
2019-01-25 01:40:27 +0530

    Fix bugs with commit event, conditional access_token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const alfy = require('alfy');
 
 // Determine the URL to open on selection of an output item.
 determineArg = (event) => {
-	const { payload: { action, comment, issue, pull_request, forkee, release }, type, repo, head, actor } = event;
+	const { payload: { action, comment, issue, pull_request, forkee, release }, type, repo, payload : { head} , actor } = event;
 	if (event.payload.action === 'created' || event.type === 'CommitCommentEvent') {
 		return comment.html_url
 	}	else if (['opened', 'closed', 'published'].includes(action)) {
@@ -12,7 +12,7 @@ determineArg = (event) => {
 	} else if (type === 'ForkEvent') {
 		return forkee.html_url
 	} else if (type === 'PushEvent') {
-		return `https://github.com/${repo.name}/commits/${head}`
+		return `https://github.com/${repo.name}/commit/${head}`
 	} else {
 		// Temporary
 		return actor.url
@@ -34,8 +34,8 @@ determineAction = event => {
 
 // Script to run on changing input
 (async () => {
-
-	const data = await alfy.fetch(`https://api.github.com/users/${alfy.input}/events?access_token=${process.env.access_token}`)
+	const { access_token } = process.env
+	const data = await alfy.fetch(`https://api.github.com/users/${alfy.input}/events${access_token ? '?access_token=' + access_token : ''}`)
 	const items =	data.map(event => {
 		// Determine the URL to open
 		const arg = determineArg(event)
